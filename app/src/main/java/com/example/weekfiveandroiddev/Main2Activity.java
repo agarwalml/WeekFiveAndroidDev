46864c4d1725ff5c409d43da3c4e189b010edfba
Mehul Agarwal <mehulagarwal@Mehuls-MBP.wv.cc.cmu.edu>
2019-10-07 23:52:00 -0400

    Finish project

--- app/src/main/java/com/example/weekfiveandroiddev/Main2Activity.java
+++ app/src/main/java/com/example/weekfiveandroiddev/Main2Activity.java
@@ -2,6 +2,8 @@ package com.example.weekfiveandroiddev;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -11,9 +13,21 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main2Activity extends AppCompatActivity {
-
+    String text = "Note 1";
+    String Body1 = "";
+    String Body2 = "";
+    String Body3 = "";
+    EditText editText1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +36,33 @@ public class Main2Activity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         final int mode = getIntent().getIntExtra("mode", 0);
+        text = getIntent().getStringExtra("text");
+        editText1 = (EditText)findViewById(R.id.edit_text1);
+        editText1.setText(text);
+
+        if(mode == 1) {
+            Body1 = open("body1");
+            EditText editText = findViewById(R.id.edit_text);
+            editText.setText(Body1);
+        }
+        else if(mode == 2) {
+            Body2 = open("body2");
+            EditText editText = findViewById(R.id.edit_text);
+            editText.setText(Body2);
+        }
+
+        else if(mode == 3) {
+            Body3 = open("body3");
+            EditText editText = findViewById(R.id.edit_text);
+            editText.setText(Body3);
+        }
+
+        else
+        {
+            Body1 = open("body1");
+            EditText editText = findViewById(R.id.edit_text);
+            editText.setText(Body1);
+        }
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -32,16 +73,93 @@ public class Main2Activity extends AppCompatActivity {
         });
     }
 
-    private void submitText(final int mode) {
-        if (mode == 2) {
-            EditText editText = findViewById(R.id.edit_text);
+    /** Writes textToSave to the file denoted by fileName. **/
+    private void save(String fileName, String textToSave) {
+        try {
+            OutputStreamWriter out =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(textToSave);
+            out.close();
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /** Checks if the file denoted by fileName exists. **/
+    private boolean fileExists(String fileName) {
+        File file = getBaseContext().getFileStreamPath(fileName);
+        return file.exists();
+    }
+
+    /** Opens the file denoted by fileName and returns the contents of the file. **/
+    private String open(String fileName) {
+        String content = "";
+        if (fileExists(fileName)) {
+            try {
+                InputStream in = openFileInput(fileName);
+                if ( in != null) {
+                    InputStreamReader tmp = new InputStreamReader( in );
+                    BufferedReader reader = new BufferedReader(tmp);
+                    String str;
+                    StringBuilder buf = new StringBuilder();
+                    while ((str = reader.readLine()) != null) {
+                        buf.append(str + "\n");
+                    } in .close();
+                    content = buf.toString();
+                }
+            } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+                Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+        return content;
+    }
+
+        private void submitText(final int mode) {
+        if (mode == 1) {
+            EditText editText1 = (EditText)findViewById(R.id.edit_text1);
             // Prepare data intent
             Intent data = new Intent();
-            // Pass relevant data back as a result
-            data.putExtra("text", editText.getText().toString());
+           // Pass relevant data back as a result
+            data.putExtra("text", editText1.getText().toString());
+
+            EditText editText = findViewById(R.id.edit_text);
+            Body1 = editText.getText().toString();
+            save("body1", Body1);
             // Activity finished ok, return the data
             setResult(RESULT_OK, data); // set result code and bundle data for response
             finish(); // closes the activity, pass data to parent
         }
-    }
+
+        else if (mode == 2) {
+                EditText editText1 = (EditText)findViewById(R.id.edit_text1);
+                // Prepare data intent
+                Intent data = new Intent();
+                // Pass relevant data back as a result
+                data.putExtra("text", editText1.getText().toString());
+
+                EditText editText = findViewById(R.id.edit_text);
+                Body2 = editText.getText().toString();
+                save("body2", Body2);
+                // Activity finished ok, return the data
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish(); // closes the activity, pass data to parent
+            }
+
+        else if (mode == 3) {
+            EditText editText1 = (EditText)findViewById(R.id.edit_text1);
+            // Prepare data intent
+            Intent data = new Intent();
+            // Pass relevant data back as a result
+            data.putExtra("text", editText1.getText().toString());
+
+            EditText editText = findViewById(R.id.edit_text);
+            Body3 = editText.getText().toString();
+            save("body3", Body3);
+            // Activity finished ok, return the data
+            setResult(RESULT_OK, data); // set result code and bundle data for response
+            finish(); // closes the activity, pass data to parent
+        }
+        }
+
 }
--- app/src/main/java/com/example/weekfiveandroiddev/MainActivity.java
+++ app/src/main/java/com/example/weekfiveandroiddev/MainActivity.java
@@ -12,11 +12,30 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int REQUEST_CODE = 20;
+    TextView title1;
+    TextView title2;
+    TextView title3;
+    String name1 = "Note 1";
+    String name2 = "Note 2";
+    String name3 = "Note 3";
+    private final int REQUEST_CODE1 = 20;
+    private final int REQUEST_CODE2 = 30;
+    private final int REQUEST_CODE3 = 40;
+
+    //ArrayList<String> items = new ArrayList<>();
+    //final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_main, R.id.title, items);
+    //ListView ListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +43,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        title1 = findViewById(R.id.title1);
+        title2 = findViewById(R.id.title2);
+        title3 = findViewById(R.id.title3);
 
+        /* ListView = (ListView) findViewById(R.id.simpleListView);
+        ListView.setAdapter(arrayAdapter);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add new Items to List
+                items.add("New Note");
+
+                    notifyDataSetChanged ()
+                        Notifies the attached observers that the underlying
+                        data has been changed and any View reflecting the
+                        data set should refresh itself.
+
+
+                arrayAdapter.notifyDataSetChanged();
+
+            }});*/
+
+
+        title1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("mode", 1); // pass arbitrary data to launched activity
+                i.putExtra("text", name1);
+                startActivityForResult(i, REQUEST_CODE1);
+            }
+        });
+
+        title2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(MainActivity.this, Main2Activity.class);
                 i.putExtra("mode", 2); // pass arbitrary data to launched activity
-                startActivityForResult(i, REQUEST_CODE);
+                i.putExtra("text", name2);
+                startActivityForResult(i, REQUEST_CODE2);
+            }
+        });
+
+        title3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("mode", 3); // pass arbitrary data to launched activity
+                i.putExtra("text", name3);
+                startActivityForResult(i, REQUEST_CODE3);
             }
         });
     }
@@ -40,11 +102,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         // REQUEST_CODE is defined above
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE1) {
             // Extract name value from result extras
-            String name = data.getExtras().getString("text");
+            name1 = data.getExtras().getString("text");
             // Toast the name to display temporarily on screen
-            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+            title1.setText(name1);
+        }
+
+        else if(resultCode == RESULT_OK && requestCode == REQUEST_CODE2) {
+            // Extract name value from result extras
+            name2 = data.getExtras().getString("text");
+            // Toast the name to display temporarily on screen
+            title2.setText(name2);
+        }
+
+        else if(resultCode == RESULT_OK && requestCode == REQUEST_CODE3) {
+            // Extract name value from result extras
+            name3 = data.getExtras().getString("text");
+            // Toast the name to display temporarily on screen
+            title3.setText(name3);
         }
     }
 
